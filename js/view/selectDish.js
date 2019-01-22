3f2d3d2e0ab6e4f61616597ea945702eb1dea561
ssssarah <mouffoksarah1@gmail.com>
2019-01-22 16:07:50 +0100

    link to dish detail

--- js/view/selectDish.js
+++ js/view/selectDish.js
@@ -14,33 +14,78 @@
  */
 var SelectDishView = function (container, model) {
 
-
 	var relativePath = "images/";
 
-	var dishList = container.find("#dishList")[0];
 	var numberOfGuests = container.find("#numberOfGuests");
 
-	var dishes = model.getAllDishes();
+	//init the select of dish types
+	var dishTypeSelect = container.find("#dishType")[0];
+	var dishTypes = model.getAllDishTypes();
+
+	for(var key in dishTypes){
+		var dishType = dishTypes[key];
+		var el = document.createElement("option");
+		el.setAttribute("value", dishType);
+		el.innerText = dishType;
+		dishTypeSelect.appendChild(el);
+	}
+
+	//populate the list of dishes
+    var dishList = container.find("#dishList")[0];
+	var type = getParameterByName("dishType");
+	var search = getParameterByName("search");
+	search = search !== "undefined" ? search : false;
+	var dishes = model.getAllDishes(type, search);
 
 	for(var key in dishes){
 		var dish = dishes[key];
 
 		var card = document.createElement("div");
 		card.classList.add("card");
-		card.classList.add("col-sm-3");
+		card.classList.add("col-sm-2");
 		card.classList.add("border");
 		card.classList.add("border-dark");
 		card.classList.add("mr-3");
 		card.style.width = "18rem";
 
-		//var item = document.createElement("div");
-
     	var img = document.createElement("img");
     	img.classList.add("card-img-top");
     	img.src =  relativePath + dish.image;
 
+    	var body = document.createElement("div");
+    	body.classList.add("card-body");
+
+    	var form = document.createElement("form");
+    	form.setAttribute("method", "get");
+    	form.setAttribute("action",  "dishDetails.html");
+
+		var id = document.createElement("input");
+		id.setAttribute("type", "hidden");
+		id.setAttribute("value", dish.id);
+		id.setAttribute("name", "dishId");
+
+    	var btn = document.createElement("button");
+    	btn.setAttribute("type", "submit");
+    	btn.classList.add("btn");
+    	btn.classList.add("btn-light");
+    	btn.innerText = dish.name;
+
+        form.appendChild(id);
+    	form.appendChild(btn);
+    	body.appendChild(form);
     	card.appendChild(img);
+    	card.appendChild(body);
     	dishList.appendChild(card);
 	}
 
 };
+
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
