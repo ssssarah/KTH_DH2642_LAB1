158f5bb222d4bffd0f29a46441bab069070b6928
ssssarah <mouffoksarah1@gmail.com>
2019-02-12 19:24:29 +0100

    fix dish detail view mistake

--- js/view/dishDetailsView.js
+++ js/view/dishDetailsView.js
@@ -16,73 +16,72 @@ let DishDetailsView = function (container, model) {
 
         console.log("dishDetail update");
         container.show();
-        
-        if (container.is(":visible")) {
-            return this.update();
-        }
+        return this.update();
 
     };
 
     this.update = function () {
+        if (container.is(":visible")) {
 
-        let id = model.getSelectedDishId();
-        return model.getDish(id).then(function (dish) {
+            let id = model.getSelectedDishId();
+            return model.getDish(id).then(function (dish) {
 
-            self.$ingredientList.empty();
-            self.$dishName.text("");
-            self.$dishImg.attr("src", "");
-            self.$dishPrep.text("");
-            self.$nbOfGuests.text("");
-            self.$price.text("");
+                self.$ingredientList.empty();
+                self.$dishName.text("");
+                self.$dishImg.attr("src", "");
+                self.$dishPrep.text("");
+                self.$nbOfGuests.text("");
+                self.$price.text("");
 
-            if(dish.error){
-                alert("Error details: " + dish.error);
-                container.show();
-                return;
-            }
+                if (dish.error) {
+                    alert("Error details: " + dish.error);
+                    container.show();
+                    return;
+                }
 
-            model.setSelectedDish(dish);
+                model.setSelectedDish(dish);
 
-            let numberOfGuests = model.getNumberOfGuests();
-            self.$dishName.text(dish.title);
-            self.$dishImg.attr("src", dish.image);
-            self.$dishPrep.text(dish.instructions);
-            self.$nbOfGuests.text(numberOfGuests);
+                let numberOfGuests = model.getNumberOfGuests();
+                self.$dishName.text(dish.title);
+                self.$dishImg.attr("src", dish.image);
+                self.$dishPrep.text(dish.instructions);
+                self.$nbOfGuests.text(numberOfGuests);
 
-            let totalPrice = 0;
+                let totalPrice = 0;
 
-            for(let key in dish.extendedIngredients) {
-                let ingredient = dish.extendedIngredients[key];
+                for (let key in dish.extendedIngredients) {
+                    let ingredient = dish.extendedIngredients[key];
 
-                let item = document.createElement("li");
-                item.classList.add("row");
+                    let item = document.createElement("li");
+                    item.classList.add("row");
 
-                let quantity = document.createElement("span");
-                quantity.classList.add("col-2");
-                quantity.innerHTML = ingredient.amount + " " + ingredient.unit;
+                    let quantity = document.createElement("span");
+                    quantity.classList.add("col-2");
+                    quantity.innerHTML = ingredient.amount + " " + ingredient.unit;
 
-                let name = document.createElement("span");
-                name.classList.add("col-6");
-                name.innerHTML = ingredient.name;
+                    let name = document.createElement("span");
+                    name.classList.add("col-6");
+                    name.innerHTML = ingredient.name;
 
-                let currency = document.createElement("span");
-                currency.classList.add("col-2");
-                currency.innerHTML = "SEK";
+                    let currency = document.createElement("span");
+                    currency.classList.add("col-2");
+                    currency.innerHTML = "SEK";
 
-                let price = document.createElement("span");
-                price.classList.add("col-2");
-                price.innerHTML = (1 * numberOfGuests).toFixed(2);
+                    let price = document.createElement("span");
+                    price.classList.add("col-2");
+                    price.innerHTML = (1 * numberOfGuests).toFixed(2);
 
-                item.appendChild(quantity);
-                item.appendChild(name);
-                item.appendChild(currency);
-                item.appendChild(price);
+                    item.appendChild(quantity);
+                    item.appendChild(name);
+                    item.appendChild(currency);
+                    item.appendChild(price);
 
-                self.$ingredientList.append(item);
-            }
+                    self.$ingredientList.append(item);
+                }
 
-            self.$price.text("SEK " + model.getDishPrice(dish).toFixed(2));
-        });
+                self.$price.text("SEK " + model.getDishPrice(dish).toFixed(2));
+            });
+        }
     };
 
     this.hide = function () {
